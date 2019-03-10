0f444b6cb9af2672c33bfabd5586c7173ebc8e1c
Anthony Morris <anthony@amorrissound.com>
2019-03-10 15:23:04 -0700

    Solve problem 12

---
+++ solutions/12/teller.js
@@ -0,0 +1,20 @@
+const jsonStream = require('duplex-json-stream');
+const net = require('net');
+
+const client = jsonStream(net.connect(3876));
+
+const id = process.argv[2];
+const cmd = process.argv[3];
+const value = process.argv[4];
+
+const cmdParams = {
+  balance: {},
+  deposit: { amount: value },
+  withdraw: { amount: value },
+};
+
+client.on('data', (msg) => {
+  console.log('Teller received:', msg);
+});
+
+client.end({ cmd, ...cmdParams[cmd], id });

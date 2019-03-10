7c16876a27492c827aba0d6bf873b21788b67322
Anthony Morris <anthony@amorrissound.com>
2019-03-10 15:26:59 -0700

    Fix register bug and issues with .gitignore

--- solutions/12/teller.js
+++ solutions/12/teller.js
@@ -4,7 +4,10 @@ const net = require('net');
 const client = jsonStream(net.connect(3876));
 
 const id = process.argv[2];
-const cmd = process.argv[3];
+// TODO: Fix terrible register hack
+const cmd = process.argv[2] === 'register'
+  ? 'register'
+  : process.argv[3];
 const value = process.argv[4];
 
 const cmdParams = {
